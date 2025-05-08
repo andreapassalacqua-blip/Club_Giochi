b165cf868e1236f6093cfce4d4790ab9619ee62f
andreapassalacqua-blip <andry.passalacqua@gmail.com>
2025-05-08 14:57:14 +0000

    #

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <body>
     <h1>Benvenuto nel Club Giochi</h1>
     <ul>
-        <li><a href="pagine/registra_socio.php">Registra un socio</a></li>
-        <li><a href="pagine/aggiungi_gioco.php">Aggiungi un gioco</a></li>
+        <li><a href="pagine/aggiungi_gioco.php">Aggiungi un gioco!</a></li>
+        <li><a href="pagine/prestito_gioco.php">Prestito di un gioco!</a></li>
         <li><a href="pagine/registra_incontro.php">Registra un incontro</a></li>
-        <li><a href="pagine/prestito_gioco.php">Prestito di un gioco</a></li>
+        <li><a href="pagine/registra_socio.php">Registra un socio!</a></li>
         <li><a href="pagine/restituzione_gioco.php">Restituzione di un gioco</a></li>
     </ul>
 </body>
